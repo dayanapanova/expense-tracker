817dfc6b6d6eee1db39c33e3bc45613f24eed2d4
Dayana Panova <dzopino@abv.bg>
2021-11-15 17:36:04 +0200

    With the help of useContext we have the transactions from the golbal state, and we use them to diplay the total amount of money, and to handle the expenses and incomes. We check if the numbers are negatiive or positive to add a class for the res or green label.

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
     return (
         <div>
             <div className="inc-exp-container">
                 <div>
-                    <h4>Income</h4>
+                    <h4>{income}</h4>
                     <p className="money plus">+$0.00</p>
                 </div>
                 <div>
-                    <h4>Expense</h4>
+                    <h4>{expense}</h4>
                     <p className="money minus">-$0.00</p>
                 </div>
             </div>
